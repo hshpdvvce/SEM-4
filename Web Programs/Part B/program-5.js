31aef43fa624a3b9f9c9ceaca84b0e1ed892de4b
hshpd <hpd.developer@gmail.com>
2025-04-23 11:40:35 +0530

    updated p5 (rectified rest operation)

--- Web Programs/Part B/program-5.js
+++ Web Programs/Part B/program-5.js
@@ -1,15 +1,11 @@
-// Develop a JavaScript program that accepts length and breadth of a rectangle as parameters of an arrow function. 
-// Call the function using spread and rest operators and alert the user with the perimeter of the rectangle.
-
-// Arrow function to calculate the perimeter of a rectangle
-const Perimeter = (length, breadth) => 2 * (length + breadth);
+// Arrow function using rest operator to calculate the perimeter of a rectangle
+const Perimeter = (...sides) => 2 * (sides[0] + sides[1]);
 
 // Using spread operator to pass parameters
 const rectangleSpread = [10, 5];
 const perimeterSpread = Perimeter(...rectangleSpread);
 console.log(`Perimeter (using spread): ${perimeterSpread}`);
 
-// Using rest operator to pass parameters
-const rectangleRest = [8, 6];
-const perimeterRest = Perimeter(...rectangleRest);
+// Using rest operator to receive parameters
+const perimeterRest = Perimeter(8, 6);
 console.log(`Perimeter (using rest): ${perimeterRest}`);
